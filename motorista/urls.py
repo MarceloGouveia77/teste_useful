6552b3872d5d0dc63c8c743317d654915f94d09b
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 02:47:50 -0300

    feat: detalhe motorista

--- motorista/urls.py
+++ motorista/urls.py
@@ -7,4 +7,5 @@ app_name = 'motorista'
 urlpatterns = [
     path('', views.listar_motoristas, name='listar_motoristas'),
     path('cadastrar/', views.cadastrar_motorista, name='cadastrar_motorista'),
+    path('detalhe/<int:pk>/', views.detalhe_motorista, name='detalhe_motorista'),
 ]
--- motorista/views.py
+++ motorista/views.py
@@ -15,3 +15,15 @@ def cadastrar_motorista(request):
         form.save()
         return HttpResponse('<script>window.location.reload()</script>')
     return render(request, 'motorista/cadastrar.html', {'form': form})
+
+def detalhe_motorista(request, pk):
+    motorista = Motorista.objects.get(id=pk)
+    movimentacoes = Movimentacao.objects.filter(motorista=motorista)
+
+    context = {
+        'motorista': motorista,
+        'movimentacoes': movimentacoes,
+        'motoristas_active': 'active'
+    }
+
+    return render(request, 'motorista/detalhe.html', context)
