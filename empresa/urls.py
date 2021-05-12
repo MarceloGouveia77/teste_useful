55a428f7d28d7bf0408202a50aaaef3cc5d19c2f
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 00:20:32 -0300

    page: listar unidades

--- empresa/urls.py
+++ empresa/urls.py
@@ -4,5 +4,6 @@ from empresa import views
 app_name = 'empresa'
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('unidades/', views.listar_unidades, name='listar_unidades')
 ]
--- empresa/views.py
+++ empresa/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from empresa.models import Unidade
 
 # Create your views here.
 
 def dashboard(request):
 
     return render(request, 'dashboard/index.html', {})
+
+def listar_unidades(request):
+    unidades = Unidade.objects.all()
+
+    return render(request, 'unidade/index.html', {'unidades': unidades})
