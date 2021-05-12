2b3a474e8534f273f2665b32bc78033cf356fdb9
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 00:40:21 -0300

    db: motorista e movimentacao

--- motorista/admin.py
+++ motorista/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from motorista.models import Motorista, Movimentacao
 # Register your models here.
+
+admin.site.register(Motorista)
+admin.site.register(Movimentacao)
--- motorista/models.py
+++ motorista/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from empresa.models import Unidade
 # Create your models here.
+
+class Motorista(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    unidade = models.ForeignKey(Unidade, related_name='Unidade', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Movimentacao(models.Model):
+    origem = models.ForeignKey(Unidade, related_name='Unidade_Origem', on_delete=models.CASCADE)
+    hora_saida = models.DateTimeField('Saida', auto_now_add=True)
+    destino = models.ForeignKey(Unidade, related_name='Unidade_Destino', on_delete=models.CASCADE)
+    hora_chegada = models.DateTimeField('Chegada', auto_now=True)
+
+    def __str__(self):
+        return f'Origem: {self.origem} / Destino: {self.destino}'
