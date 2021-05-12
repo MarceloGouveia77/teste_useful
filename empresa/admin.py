3c7acf28b910a9fa577877e6f0885fda4f95e5e8
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-11 23:49:25 -0300

    db: unidade empresa

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from empresa.models import Unidade
 
 # Register your models here.
+
+admin.site.register(Unidade)
--- empresa/models.py
+++ empresa/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Unidade(models.Model):
+    nome = models.CharField('Nome', max_length=100, blank=False)
+    cidade = models.CharField('Cidade', max_length=50)
+    rua = models.CharField('Rua', max_length=50)
+    numero = models.CharField('Numero', max_length=50)
+    codigo_postal = models.CharField('Código Postal', max_length=50)
+
+    def __str__(self):
+        return self.nome
