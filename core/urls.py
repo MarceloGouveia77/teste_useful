488300b55574ffa44667110bc8ed25be0f52ae88
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 03:55:42 -0300

    feat: remover motorista e unidade

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('remover_registro/<int:pk>/<str:app>/<str:model>/', views.remover_registro, name='remover_registro')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.apps import apps
 
 # Create your views here.
 
 def index(request):
 
     return render(request, 'core/base.html', {})
+
+def remover_registro(request, pk, app, model):
+    if request.method == "POST":
+        Model = apps.get_model(app, model)
+        instance = Model.objects.get(id=pk)
+        instance.delete()
+        return HttpResponse('<script>window.location = document.referrer;</script>') # RETORNA PRA PÁGINA ANTERIOR
+    return render(request, 'core/remover_registro.html', {'pk': pk, 'app': app, 'model': model})
